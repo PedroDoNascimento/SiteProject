9525cc75c901fe4982845f1b73f9ca1134454a5f
Pedro Nasc <162610617+PedroDoNascimento@users.noreply.github.com>
2024-05-03 20:38:10 -0300

    Structural repairs

--- Codes/styles/cadastro.js
+++ Codes/styles/cadastro.js
@@ -1,13 +1,10 @@
 // cadastro.js
-
 function logar() {
     var usuario = document.getElementById('user').value;
     var senha = document.getElementById('senha').value;
 
-    // Obtém a lista de usuários cadastrados do localStorage
     var usuariosCadastrados = JSON.parse(localStorage.getItem('usuarios'));
 
-    // Verifica se o usuário e a senha estão corretos
     var usuarioExistente = usuariosCadastrados.find(function (user) {
         return user.nome === usuario && user.senha === senha;
     });
